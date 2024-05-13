22a985e1ab03f9c171b12eec5b1ec5ee67d81d6f
Coca <coca16622@gmail.com>
2024-05-13 13:08:50 +0300

    Add Lix

--- flake.nix
+++ flake.nix
@@ -10,6 +10,17 @@
     lanzaboote.url = "github:nix-community/lanzaboote/v0.3.0";
     lanzaboote.inputs.nixpkgs.follows = "nixpkgs";
 
+    lix = {
+      url = "git+https://git@git.lix.systems/lix-project/lix?ref=refs/tags/2.90-beta.1";
+      flake = false;
+    };
+
+    lix-module = {
+      url = "git+https://git.lix.systems/lix-project/nixos-module";
+      inputs.lix.follows = "lix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     # nix-gaming.url = "github:fufexan/nix-gaming";
     # nix-gaming.inputs.nixpkgs.follows = "nixpkgs";
 
@@ -23,6 +34,7 @@
   outputs = {
     nixpkgs,
     home-manager,
+    lix-module,
     ...
   } @ inputs: let
     system = "x86_64-linux";
@@ -38,6 +50,7 @@
         specialArgs = {inherit lanzaboote nix-gaming nix-minecraft nixos-cosmic;};
 
         modules = [
+          lix-module.nixosModules.default
           ./modules/cosmic.nix
           ./modules/minecraft.nix
           ./modules/secureboot.nix
