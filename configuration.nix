168b5ba87abab2d6c36f2dab608760970db1fb86
Coca <coca16622@gmail.com>
2024-06-25 06:14:45 +0300

    Update flake inputs

--- configuration.nix
+++ configuration.nix
@@ -233,10 +233,9 @@
   };
 
   # Enable OpenGL
-  hardware.opengl = {
+  hardware.graphics = {
     enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
+    enable32Bit = true;
   };
 
   # Load nvidia driver for Xorg and Wayland
