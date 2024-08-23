42179abbfb643dff1ec6d0a4a8def76f9ff74d5d
Coca <coca16622@gmail.com>
2024-08-23 16:08:32 +0300

    Update flake inputs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Yippeee";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
