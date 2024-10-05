a53603b06f9f6026b10e844f953334017308f225
Coca <coca16622@gmail.com>
2024-10-05 04:49:38 +0300

    Add nvidia and thermald to nicetop

--- nicetop/default.nix
+++ nicetop/default.nix
@@ -28,6 +28,35 @@
 
   networking.hostName = "nicetop"; # Define your hostname.
 
+  services.thermald.enable = true;
+
+  environment.systemPackages = with pkgs; [nvtopPackages.nvidia];
+
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    # Enables proprietary drivers
+    modesetting.enable = true;
+
+    # Open is the new default for 560 (beta) drivers
+    open = false;
+
+    # Enables settings gui which barely works
+    nvidiaSettings = false;
+
+    prime = {
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+    };
+
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
