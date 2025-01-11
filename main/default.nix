f9a44c28a82857767c7d4ff0c830d9ebf24667a0
Coca <coca16622@gmail.com>
2025-01-11 09:22:55 +0200

    Revert "Add mediawiki testing instance"
    This reverts commit a71df2020d419d3e44d26f6fd9b7d63008504dfe.

--- main/default.nix
+++ main/default.nix
@@ -14,66 +14,6 @@
 
   environment.systemPackages = with pkgs; [nvtopPackages.nvidia virtiofsd];
 
-  services.mediawiki = let
-    old_pkg =
-      import (builtins.fetchTarball {
-        name = "nixpkg-working-mediawiki";
-        url = "https://github.com/nixos/nixpkgs/archive/571c71e6f73af34a229414f51585738894211408.tar.gz";
-        sha256 = "0fgp5sqfmh5zgx75rs5101ywkz0fkjff67abms0kc8hyaxmlc7js";
-      }) {
-        system = "x86_64-linux";
-      };
-  in {
-    enable = true;
-    package = old_pkg.mediawiki;
-    name = "Sample MediaWiki";
-    httpd.virtualHost = {
-      hostName = "localhost";
-      adminAddr = "admin@example.com";
-    };
-    passwordFile = pkgs.writeText "password" "cardbotnine";
-
-    extraConfig = ''
-      $wgMaxUploadSize = 1000000000;
-      $wgAllowCopyUploads = true;
-      $wgScribuntoEngineConf['luastandalone']['luaPath'] = '${pkgs.lua51Packages.lua}/bin/lua';
-    '';
-
-    extensions = {
-      VisualEditor = null;
-
-      PortableInfobox = pkgs.fetchzip {
-        url = "https://github.com/Universal-Omega/PortableInfobox/archive/f5780412fcb25d3981cdc7f2af8f75518d9ee3cb.zip";
-        hash = "sha256-Hm1+jzhq+PIx699ICgJM92xt6UW8jko+kxT2icdCDFc=";
-      };
-
-      Scribunto = pkgs.fetchzip {
-        url = "https://extdist.wmflabs.org/dist/extensions/Scribunto-REL1_42-9c46437.tar.gz";
-        hash = "sha256-oX4FOAJqe6G7AzxcT2ANsxoA93M6JSl/O+kudi/LQUA=";
-      };
-
-      Capiunto = pkgs.fetchzip {
-        url = "https://extdist.wmflabs.org/dist/extensions/Capiunto-REL1_42-77ccef8.tar.gz";
-        hash = "sha256-wKRqzbFSiB9xZmz293S1gYUtQyax/ikxWVc0+gjwCdY=";
-      };
-
-      TabberNeue = pkgs.fetchzip {
-        url = "https://github.com/StarCitizenTools/mediawiki-extensions-TabberNeue/archive/a003ee0787bd9033498caf933aaf8c1acde64489.zip";
-        hash = "sha256-INqaJhKIyci1NiI6GAiP1QXHHtkwGjV9jtFpGunP8Fc=";
-      };
-
-      Array = pkgs.fetchzip {
-        url = "https://extdist.wmflabs.org/dist/extensions/Arrays-REL1_42-41a1be4.tar.gz";
-        hash = "sha256-8GnG6u4jpA6PpYe1rT5uPKtW33E7LhTvi9bWbJFyjR4=";
-      };
-    };
-  };
-
-  services.phpfpm.pools.mediawiki.phpOptions = ''
-    upload_max_filesize = 150000M
-    post_max_size = 1500000M
-  '';
-
   # Cuda
   nixpkgs.config.cudaSupport = true;
   nixpkgs.overlays = [
