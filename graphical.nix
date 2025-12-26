1c91547afb91ca22c9d3ca223efdb3d87b092c1a
Coca <coca16622@gmail.com>
2025-12-26 01:39:30 +0000

    Have ctrl to click on links in alacritty, why is this not the default??

--- graphical.nix
+++ graphical.nix
@@ -382,9 +382,23 @@ in {
           ZELLIJ_AUTO_ATTACH = "true";
           ENABLE_ZELLIJ = "true";
         };
-        terminal.shell.program = "${pkgs.fish}/bin/fish";
+        terminal.shell.program = lib.getExe pkgs.fish;
         window.opacity = 0.85;
         window.decorations = "None";
+        # Insane that the default opens links just by clicking on them??
+        hints.enabled = [
+          {
+            command = "xdg-open";
+            hyperlinks = true;
+            post_processing = true;
+            persist = false;
+            mouse.enabled = true;
+            mouse.mods = "Control";
+            binding.key = "O";
+            binding.mods = "Control|Shift";
+            regex = "(ipfs:|ipns:|magnet:|mailto:|gemini:|gopher:|https:|http:|news:|file:|git:|ssh:|ftp:)[^\\u0000-\\u001F\\u007F-\\u009F<>\"\\\\s{-}\\\\^⟨⟩`]+";
+          }
+        ];
       };
     };
   };
