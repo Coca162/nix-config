d7ffa6641ef30424cf30942cfcb05add1cc36ea9
Coca <me@coca.codes>
2026-03-13 14:21:14 +0000

    Update lockfile
    Updated nixpkgs, home-manager and rust-overlay

--- packages/default.nix
+++ packages/default.nix
@@ -5,13 +5,18 @@ final: prev: {
     thumbfast-osc = import ./thumbfast-osc.nix final.pkgs;
   });
 
-  rescrobbled = prev.rescrobbled.overrideAttrs (old: {
+  rescrobbled = prev.rescrobbled.overrideAttrs (old: rec {
     version = "auto-reconnect";
     src = final.fetchFromGitHub {
       owner = "marius851000";
       repo = "rescrobbled";
       rev = "1fc643b888c8ad2eb46c53a25b6f8f1da4f38b3d";
       hash = "sha256-OXLJvPwEWqrzRdEZlBv6eb3TfVaA7ujbAAoeFq2BHK4=";
+    };
+    cargoDeps = prev.rustPlatform.fetchCargoVendor {
+      inherit src;
+      name = "rescrobbled-${version}-vendor";
+      hash = "sha256-ZawdZdP87X7xMdSdZ1VJDJxz7dBGVYo+8jR8qb2Jgq8=";
     };
   });
 }
