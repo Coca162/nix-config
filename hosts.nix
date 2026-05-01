0517dcb4456fffa8f72bc6bb7c2264e68fa9d1de
Coca <me@coca.codes>
2026-05-01 21:38:19 +0100

    Micro optimization

--- hosts.nix
+++ hosts.nix
@@ -1,30 +1,32 @@
 let
   sources = import ./npins;
 
-  lib = import "${sources.nixpkgs}/lib";
-  pkgs = import sources.nixpkgs {
-    overlays = [(import ./packages)];
+  pkgs = let
+    inherit (pkgs.lib) getName teams licenses;
+  in
+    import sources.nixpkgs {
+      overlays = [(import ./packages)];
 
-    config.allowUnfreePredicate = pkg:
-      builtins.elem (lib.getName pkg) [
-        "nvidia-x11"
-        "osu-lazer-bin"
-        "bitwig-studio-unwrapped"
-        "bitwig-studio6"
-        "bitwig-studio6-6.0"
-        "aseprite" # Source available
-        "obsidian"
-        "reaper"
-        "steam"
-        "steam-unwrapped"
-      ]
-      # TODO: Find a better way to do this
-      || ((pkg.meta ? teams) && pkg.meta.teams == [lib.teams.cuda]);
-    config.allowlistedLicenses = [lib.licenses.nvidiaCuda];
-  };
+      config.allowUnfreePredicate = pkg:
+        builtins.elem (getName pkg) [
+          "nvidia-x11"
+          "osu-lazer-bin"
+          "bitwig-studio-unwrapped"
+          "bitwig-studio6"
+          "bitwig-studio6-6.0"
+          "aseprite" # Source available
+          "obsidian"
+          "reaper"
+          "steam"
+          "steam-unwrapped"
+        ]
+        # TODO: Find a better way to do this
+        || ((pkg.meta ? teams) && pkg.meta.teams == [teams.cuda]);
+      config.allowlistedLicenses = [licenses.nvidiaCuda];
+    };
 
   nixosSystem = import "${sources.nixpkgs}/nixos/lib/eval-config.nix";
-  recursivelyImport = import ./recursivelyImport.nix {inherit lib;};
+  recursivelyImport = import ./recursivelyImport.nix {inherit (pkgs) lib;};
 
   specialArgs = {
     inherit sources;
