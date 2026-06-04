3c8645c86f7acd8a5cce07555ab7409e88cbba9d
Coca <me@coca.codes>
2026-06-04 17:40:29 +0100

    xdg-ninja nightmare again

--- base/programs.nix
+++ base/programs.nix
@@ -52,12 +52,19 @@
   programs.command-not-found.enable = false;
   environment.variables.MANPAGER = "${lib.getExe pkgs.bat} --wrap=auto --language=man --plain --strip-ansi=auto";
 
-  hm.home.sessionVariables = {
-    _JAVA_OPTIONS = "-Djava.util.prefs.userRoot=${config.hm.xdg.configHome}/java";
-    DOTNET_CLI_HOME = "${config.hm.xdg.dataHome}/dotnet";
-    CUDA_CACHE_PATH = "${config.hm.xdg.cacheHome}/nv";
-    XCOMPOSECACHE = "${config.hm.xdg.cacheHome}/X11/xcompose";
-    NUGET_PACKAGES = "${config.hm.xdg.cacheHome}/NuGetPackages";
+  environment.sessionVariables = {
+    XDG_CACHE_HOME = "$HOME/.cache";
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_DATA_HOME = "$HOME/.local/share";
+    XDG_STATE_HOME = "$HOME/.local/state";
+    _JAVA_OPTIONS = "-Djava.util.prefs.userRoot=\${XDG_CONFIG_HOME}/java";
+    DOTNET_CLI_HOME = "$XDG_DATA_HOME/dotnet";
+    CUDA_CACHE_PATH = "$XDG_CACHE_HOME/nv";
+    XCOMPOSECACHE = "$XDG_CACHE_HOME/X11/xcompose";
+    NUGET_PACKAGES = "$XDG_CACHE_HOME/NuGetPackages";
+    ANDROID_USER_HOME = "$XDG_DATA_HOME/android";
+    PARALLEL_HOME = "$XDG_CACHE_HOME/parallel";
+    GTK2_RC_FILES = "$XDG_CONFIG_HOME/gtk-2.0/gtkrc";
   };
 
   hm.programs.micro.enable = true;
