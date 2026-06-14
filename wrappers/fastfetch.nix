69aa93bfa71f41dbdbe6a8a34a3cae69a7f5c0da
Coca <me@coca.codes>
2026-06-14 16:47:57 +0100

    fastfetch relax

--- wrappers/fastfetch.nix
+++ wrappers/fastfetch.nix
@@ -31,4 +31,19 @@ _: {
       "colors"
     ];
   };
+
+  options.package.defaultFunc = {inputs}:
+    inputs.nixpkgs.pkgs.fastfetch.minimal.override {
+      audioSupport = true;
+      brightnessSupport = true;
+      codecSupport = true;
+      dbusSupport = true;
+      imageSupport = true;
+      openclSupport = true;
+      openglSupport = true;
+      sqliteSupport = true;
+      terminalSupport = true;
+      vulkanSupport = true;
+      waylandSupport = true;
+    };
 }
