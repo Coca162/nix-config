aaea931965cee25f7a7ca6900417bea50a72f107
Coca <me@coca.codes>
2026-06-30 09:42:03 +0100

    Update lockfile
    Updated nixpkgs, rust-overlay and adios

--- base/bootloader.nix
+++ base/bootloader.nix
@@ -1,6 +1,5 @@
 {pkgs, ...}: {
   boot.kernelPackages = pkgs.linuxPackages_zen;
-  system.boot.loader.kernelFile = "vmlinuz";
 
   boot.supportedFilesystems = ["ntfs"];
   boot.kernel.sysctl."kernel.sysrq" = 1;
