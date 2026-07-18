809e0279973efdea7346107209a5fc619aa912be
Coca <me@coca.codes>
2026-07-18 16:48:09 +0100

    Update lockfile
    Updated nixpkgs, rust-overlay, lix, adios and adios-wrappers
    
    Due to
    https://git.lix.systems/lix-project/lix/commit/341c0a1479f72fc6b623024c561249483f5dc663
    nil has to be patched since it depends on the docs for some reason on
    tests

--- graphical/vscodium.nix
+++ graphical/vscodium.nix
@@ -4,6 +4,52 @@
   lib,
   ...
 }: let
+  nil = pkgs.nil.overrideAttrs {
+    patches = [
+      (builtins.toFile
+        "nix-doc-update.patch"
+        ''
+          diff --git a/crates/builtin/src/lib.rs b/crates/builtin/src/lib.rs
+          index 719fe2b..f56ed19 100644
+          --- a/crates/builtin/src/lib.rs
+          +++ b/crates/builtin/src/lib.rs
+          @@ -47,7 +47,9 @@ mod tests {
+                               "\
+           Return the names of the attributes in the set *set* in an
+           alphabetically sorted list. For instance, `builtins.attrNames { y
+          -= 1; x = \"foo\"; }` evaluates to `[ \"x\" \"y\" ]`.\
+          += 1; x = \"foo\"; }` evaluates to `[ \"x\" \"y\" ]`.
+          +
+          +Has `O(l n log n)` time complexity, where `n` is number of attributes in the *set* and `l` is the maximum attribute name length.\
+                           "
+                           ),
+                           impure_only: false,
+          diff --git a/crates/ide/src/ide/hover.rs b/crates/ide/src/ide/hover.rs
+          index 535bfde..09a82b0 100644
+          --- a/crates/ide/src/ide/hover.rs
+          +++ b/crates/ide/src/ide/hover.rs
+          @@ -387,6 +387,8 @@ mod tests {
+                           Return the first element of a list; abort evaluation if the argument
+                           isn’t a list or is an empty list. You can test whether a list is
+                           empty by comparing it with `[]`.
+          +
+          +                Has constant time complexity.
+                       "#]],
+                   );
+               }
+          @@ -404,6 +406,8 @@ mod tests {
+                           Return the first element of a list; abort evaluation if the argument
+                           isn’t a list or is an empty list. You can test whether a list is
+                           empty by comparing it with `[]`.
+          +
+          +                Has constant time complexity.
+                       "#]],
+                   );
+               }
+        '')
+    ];
+  };
+
   settings = {
     "diffEditor.ignoreTrimWhitespace" = false;
     "editor.fontFamily" = "Cascadia Code";
@@ -13,7 +59,7 @@
     "git.autofetch" = false;
     "nix.enableLanguageServer" = true;
     # TODO: Don't reference these directly?
-    "nix.serverPath" = "${lib.getExe pkgs.nil}";
+    "nix.serverPath" = "${lib.getExe nil}";
     "nix.serverSettings".nil.formatting.command = [
       "${lib.getExe pkgs.alejandra}"
       "--"
