c497acf442fe87cc16cecac3e9ffca7b84138b63
Coca <me@coca.codes>
2026-07-30 06:13:59 +0100

    Update lockfile
    Updated nixpkgs, rust-overlay, lix and adios-wrappers

--- base/programs.nix
+++ base/programs.nix
@@ -46,24 +46,9 @@
       hyperfine
       lazygit
       fzf
+      nix-output-monitor
     ]
     ++ [
-      (
-        pkgs.nix-output-monitor.overrideAttrs (prev: {
-          version = "0-unstable-2026-06-08";
-          src = pkgs.fetchFromGitHub {
-            owner = "maralorn";
-            repo = "nix-output-monitor";
-            rev = "388f56120f655a9cf4512e697b2c2afa06fe7434";
-            hash = "sha256-3N+PVFpsnBtQ11Vk9OKm1q9dE0d5fxGsEDyfwoxpYaE=";
-          };
-          propagatedBuildInputs =
-            (prev.propagatedBuildInputs or [])
-            ++ [
-              pkgs.haskellPackages.hinotify
-            ];
-        })
-      )
       wrappers.zoxide
       wrappers.tealdeer
       wrappers.jujutsu
--- wrappers/niri/default.nix
+++ wrappers/niri/default.nix
@@ -1,19 +1,3 @@
 _: {
   options.configFile.default = builtins.toString ./config.kdl;
-
-  options.package.defaultFunc = {inputs}: let
-    inherit (inputs.nixpkgs.pkgs) niri libdisplay-info fetchFromGitLab;
-  in
-    niri.override {
-      libdisplay-info = libdisplay-info.overrideAttrs (finalAttrs: {
-        version = "0.3.0";
-        src = fetchFromGitLab {
-          domain = "gitlab.freedesktop.org";
-          owner = "emersion";
-          repo = "libdisplay-info";
-          rev = finalAttrs.version;
-          sha256 = "sha256-nXf2KGovNKvcchlHlzKBkAOeySMJXgxMpbi5z9gLrdc=";
-        };
-      });
-    };
 }
