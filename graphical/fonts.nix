e5e1ffa58b0c870755362a59d16d7403d88903c8
Coca <me@coca.codes>
2026-08-07 11:55:53 +0100

    Fix braille and remove redundant font

--- graphical/fonts.nix
+++ graphical/fonts.nix
@@ -1,8 +1,12 @@
 {pkgs, ...}: {
   fonts.packages = with pkgs; [
     google-fonts # EVER FONT IN EXISTENCE!!!
-    cascadia-code
     monocraft
     miracode
+  ];
+
+  fonts.fontconfig.defaultFonts.monospace = [
+    "DejaVu Sans Mono"
+    "Cascadia Mono"
   ];
 }
