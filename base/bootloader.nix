5f00f580c7182f5fc13b0831a7ae24c0c1d92ea1
Coca <me@coca.codes>
2026-09-01 17:46:43 +0100

    Use custom kernel and new nvidia drivers

--- base/bootloader.nix
+++ base/bootloader.nix
@@ -1,5 +1,36 @@
-{pkgs, ...}: {
-  boot.kernelPackages = pkgs.linuxPackages_zen;
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  # kernel = pkgs.callPackage ./_zen.nix {};
+  kernel = pkgs.linuxKernel.manualConfig rec {
+    pname = "linux-zen";
+    version = "7.2.2";
+    modDirVersion = lib.versions.pad 3 "${version}-zen1";
+    configfile = ./kernel.config;
+    isZen = true;
+    features.efiBootStub = true;
+    features.ia32Emulation = true;
+    src = pkgs.fetchFromGitHub {
+      owner = "zen-kernel";
+      repo = "zen-kernel";
+      rev = "v7.2.2-zen1";
+      hash = "sha256-KDPxfU+md/ii13KfwSz6IocPFma8Xe0T3xXlsIEj0GM=";
+    };
+
+    extraMakeFlags = [
+      "KCFLAGS+=-march=tigerlake"
+      "KCFLAGS+=-mtune=tigerlake"
+      "KCPPFLAGS+=-march=tigerlake"
+      "KCPPFLAGS+=-mtune=tigerlake"
+    ];
+  };
+in {
+  boot.kernelPackages = lib.recurseIntoAttrs (pkgs.linuxKernel.packagesFor kernel);
+
+  # for custom kernel easiness
+  boot.initrd.includeDefaultModules = false;
 
   boot.supportedFilesystems = ["ntfs"];
   boot.kernel.sysctl."kernel.sysrq" = 1;
--- hosts/nicetop/graphics.nix
+++ hosts/nicetop/graphics.nix
@@ -12,10 +12,10 @@
   # For when the beta drivers are broken with the later kernel version
   # https://www.nvidia.com/en-us/drivers/unix/
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-    version = "595.84"; # Copy the version number here
-    sha256_64bit = "sha256-mcQE5SExvye8ptoCaNzOPr7cenOrF0BxqZXPGmxeugY="; # "Cannot build "/nix/store/*-nvidia-x11-*"
+    version = "595.99.02"; # Copy the version number here
+    sha256_64bit = "sha256-6HR3lYv3YwcFSTJL1a1slI66btIQ5EAFs+/4SUD24ew="; # "Cannot build "/nix/store/*-nvidia-x11-*"
     sha256_aarch64 = "";
-    openSha256 = "sha256-pEmA2tUcOKwUPKy6N0QvS49Pdut4/7Phs/JhjdyBcNY="; # "Cannot build "/nix/store/*-nvidia-open-*"
+    openSha256 = "sha256-T36x/jx8yQ8l3LFp1rZIrTfcSwbGy8YSAvXOUSptpb4="; # "Cannot build "/nix/store/*-nvidia-open-*"
     settingsSha256 = ""; # "Cannot build "/nix/store/*-nvidia-settings-*"
     persistencedSha256 = "";
   };
